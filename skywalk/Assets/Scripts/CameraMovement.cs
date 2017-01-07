3c5803ff49cdfc95c93f71c8747fba7509560ada
shuningzhou <peterzhou43@gmail.com>
2017-01-06 22:17:12 -0800

    initial commit

---
+++ skywalk/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	public GameObject followTarget;
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (followTarget != null) {
+			CharacterMovement moveScript = followTarget.GetComponent<CharacterMovement>();
+			Vector3 position = moveScript.getFootPosition();
+
+			position.x = position.x + 0;
+			position.y = position.y + 0;
+			position.z = position.z + 0;
+
+			transform.position = Vector3.Lerp(transform.position,
+				position, Time.deltaTime * moveSpeed);
+		}
+	}
+}
---
+++ skywalk/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+	public float rotateSpeed = 250.0f;
+	public GameObject leftFoot;
+	public GameObject rightFoot;
+	bool rightInFront = false;
+
+	public Vector3 getFootPosition()
+	{
+		if (rightFoot != null && leftFoot != null) {
+			if (rightInFront) {
+				return rightFoot.transform.position;
+			} else {
+				return leftFoot.transform.position;
+			}
+		} else {
+			return Vector3.zero;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	}
+
+	void doTurn() {
+		rightInFront = !rightInFront;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (rightFoot != null && leftFoot != null) {
+			if (Input.GetButtonUp ("Horizontal")) {
+				doTurn ();
+			}
+
+			if (rightInFront) {
+
+				//rotate around the right foot
+				transform.RotateAround (rightFoot.transform.position, Vector3.down, rotateSpeed * Time.deltaTime);
+
+			} else {
+
+				//rotate around the right foot
+				transform.RotateAround (leftFoot.transform.position, Vector3.up, rotateSpeed * Time.deltaTime);
+
+			}
+		}
+	}
+
+
+}
