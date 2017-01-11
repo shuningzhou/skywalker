68c9e9242fc9f61fad90748c78fd467e6bb95327
shuningzhou <peterzhou43@gmail.com>
2017-01-10 22:39:29 -0800

    initial work for RoadGenerator

---
+++ skywalk/Assets/Scripts/RoadGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGenerator : MonoBehaviour {
+	public GameObject roadSection;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
