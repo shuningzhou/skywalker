60c74f520969f78ef17b6c3b2a4d3a6aebe8f9a5
Biao Yu <biaoyu@Mac-Pro.vc.shawcable.net>
2017-01-18 23:02:46 -0800

    created building generator script

---
+++ skywalk/Assets/Scripts/BuildingGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGenerator : MonoBehaviour {
+
+	public int currentMatrixIndex_x;
+	public int currentMatrixIndex_y;
+//	private GameObject matrix[100][100];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
