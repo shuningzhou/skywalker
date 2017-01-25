55a97795a1da4b08971d35e1528632fae14d76c4
shuningzhou <peterzhou43@gmail.com>
2017-01-25 00:42:06 -0800

    part 5

--- skywalk/Assets/Scripts/EveryPlayHelper.cs
+++ skywalk/Assets/Scripts/EveryPlayHelper.cs
@@ -131,16 +131,16 @@ public class EveryPlayHelper : MonoBehaviour
 
 	void OnGUI()
 	{
-		int w = Screen.width, h = Screen.height;
-
-		GUIStyle style = new GUIStyle();
-
-		Rect rect = new Rect(0, 0, w, h * 2 / 100);
-		style.alignment = TextAnchor.UpperLeft;
-		style.fontSize = h * 2 / 100;
-		style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
-		string text = debugText;
-		GUI.Label(rect, text, style);
+//		int w = Screen.width, h = Screen.height;
+//
+//		GUIStyle style = new GUIStyle();
+//
+//		Rect rect = new Rect(0, 0, w, h * 2 / 100);
+//		style.alignment = TextAnchor.UpperLeft;
+//		style.fontSize = h * 2 / 100;
+//		style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
+//		string text = debugText;
+//		GUI.Label(rect, text, style);
 	}
 
 
