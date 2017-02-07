37e647a8a6c20b186a5a0fb604cab3ba561be020
Biao Yu <yubiaohot@hotmail.com>
2017-02-07 00:01:33 -0800

    created skill class

---
+++ skywalk/Assets/Scripts/Skill.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour {
+
+	enum skillID {
+		RoadWiden = 0,
+		AutoColect = 1,
+		StepOverAir = 2,
+		IncreaseSize = 3,
+	};//the enum value is the hardcoded skill getting order
+
+	struct skillInfo{
+		int level;
+		string description;
+		skillID id;
+		int coinsToUpgrade;
+		bool playerOrGame;
+	};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public virtual void Activate(GameObject myobject){
+		
+	}
+}
