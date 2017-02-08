66ae0428a05d6da5f86805bd100a8201699d7898
Biao Yu <yubiaohot@hotmail.com>
2017-02-08 00:13:41 -0800

    Added basic APIs to Skill script

--- skywalk/Assets/Scripts/Skill.cs
+++ skywalk/Assets/Scripts/Skill.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class Skill : MonoBehaviour {
 
-	enum skillID {
+	public enum skillID {
 		RoadWiden = 0,
 		AutoColect = 1,
 		StepOverAir = 2,
 		IncreaseSize = 3,
 	};//the enum value is the hardcoded skill getting order
 
-	struct skillInfo{
-		int level;
-		string description;
-		skillID id;
-		int coinsToUpgrade;
-		bool playerOrGame;
+	public struct skillInfo
+	{
+		public int level;
+		public string description;
+		public skillID id;
+		public int coinsToUpgrade;
+		public bool playerOrGame;
 	};
+
+	public struct skillTimer
+	{
+		public float startTime;
+		public float lastTime;
+		public float endTime;
+	};
+
+//	public static Skill Instance = null;
+	public skillInfo thisSkillInfo = new skillInfo();
+
+//	void Awake () {
+//		if (Instance == null) {
+//			Instance = this;
+//		} else if (Instance != this) {
+//			Destroy(gameObject);
+//		}
+//	}
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -31,5 +50,42 @@ public class Skill : MonoBehaviour {
 
 	public virtual void Activate(GameObject myobject){
 		
+	}
+
+	public virtual skillInfo GetSkillInfo(){
+		return thisSkillInfo;
+	}
+
+	public virtual int SkillLevelUpgrade(skillID id){
+		if (thisSkillInfo.id == id) {
+			if (thisSkillInfo.level == 0) {
+				// If skill is not unlocked, we don't upgrade it
+			} else {
+				thisSkillInfo.level++;
+			}
+			return thisSkillInfo.level;
+		} else {
+			// id is invalid
+			return -1;
+		}
+	}
+
+	public virtual bool IsSkillUnlocked(){
+		if (thisSkillInfo.level > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public virtual bool UnlockSkill(skillID id){
+		if (thisSkillInfo.id == id) {
+			if (thisSkillInfo.level == 0) {
+				thisSkillInfo.level++;
+			}
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
