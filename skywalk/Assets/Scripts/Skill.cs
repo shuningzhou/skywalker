25f1c23abe1f9451b6a570ca321def7740e6f53d
Biao Yu <yubiaohot@hotmail.com>
2017-02-11 17:20:04 -0800

    Added the skill objects. Added hashtable to hardcode level vs. coins, level vs. drops

--- skywalk/Assets/Scripts/Skill.cs
+++ skywalk/Assets/Scripts/Skill.cs
@@ -4,31 +4,55 @@ using UnityEngine;
 
 public class Skill : MonoBehaviour {
 
+	public Hashtable duration_level_coin_table;
+
+	public Hashtable duration_level_time_table;
+
+	public Hashtable droplets_level_coin_table;
+
+	public Hashtable droplets_level_drops_table;
+
 	public enum skillID {
-		RoadWiden = 0,
-		AutoColect = 1,
-		StepOverAir = 2,
-		IncreaseSize = 3,
+		Haste = 0,
+		Levitation = 1,
+		Growth = 2,
+		Magnet = 3,
+		Conjure = 4,
 	};//the enum value is the hardcoded skill getting order
 
-	public struct skillInfo
+	public enum levelID{
+		Duration = 0,
+		Droplets,
+	};
+
+	public struct skillLevel
 	{
-		public int level;
-		public string description;
-		public skillID id;
-		public int coinsToUpgrade;
-		public bool playerOrGame;
+		public int durationLevel;
+		public int requiredConinsDurationLvl;
+		public int dropletsLevel;
+		public int reuqiredConinsDropletLvl;
 	};
 
 	public struct skillTimer
 	{
 		public float startTime;
-		public float lastTime;
+		public float durationTime;
 		public float endTime;
 	};
 
-//	public static Skill Instance = null;
+	public struct skillInfo
+	{
+		public skillID id;
+		public skillLevel level;
+		public string description;
+		public bool playerOrGame;
+		public bool isActivate;
+		public skillTimer timer;
+		public int requiredDroplets;
+	};
+
 	public skillInfo thisSkillInfo = new skillInfo();
+//	public static Skill Instance = null;
 
 //	void Awake () {
 //		if (Instance == null) {
@@ -56,22 +80,44 @@ public class Skill : MonoBehaviour {
 		return thisSkillInfo;
 	}
 
-	public virtual int SkillLevelUpgrade(skillID id){
-		if (thisSkillInfo.id == id) {
-			if (thisSkillInfo.level == 0) {
-				// If skill is not unlocked, we don't upgrade it
+	public virtual int SkillLevelUpgrade(skillID skill_id, levelID level_id){
+		if (level_id == levelID.Duration) {
+			if (thisSkillInfo.id == skill_id) {
+				if (thisSkillInfo.level.durationLevel == 0 || thisSkillInfo.level.durationLevel == 10) {
+					// If skill is not unlocked, or if skill reaches the max level,
+					// we don't upgrade it
+				} else {
+					thisSkillInfo.level.durationLevel++;
+					thisSkillInfo.timer.durationTime = (float)duration_level_time_table [thisSkillInfo.level.durationLevel];
+					thisSkillInfo.level.requiredConinsDurationLvl = (int)duration_level_coin_table [thisSkillInfo.level.durationLevel];
+				}
+				return thisSkillInfo.level.durationLevel;
 			} else {
-				thisSkillInfo.level++;
+				// id is invalid
+				return -1;
 			}
-			return thisSkillInfo.level;
+		} else if (level_id == levelID.Droplets) {
+			if (thisSkillInfo.id == skill_id) {
+				if (thisSkillInfo.level.dropletsLevel == 0 || thisSkillInfo.level.dropletsLevel == 5) {
+					// If skill is not unlocked, or if skill reaches the max level,
+					// we don't upgrade it
+				} else {
+					thisSkillInfo.level.dropletsLevel++;
+					thisSkillInfo.requiredDroplets = (int)droplets_level_drops_table [thisSkillInfo.level.dropletsLevel];
+					thisSkillInfo.level.requiredConinsDurationLvl = (int)droplets_level_coin_table [thisSkillInfo.level.dropletsLevel];
+				}
+				return thisSkillInfo.level.dropletsLevel;
+			} else {
+				// id is invalid
+				return -1;
+			}
 		} else {
-			// id is invalid
 			return -1;
 		}
 	}
 
 	public virtual bool IsSkillUnlocked(){
-		if (thisSkillInfo.level > 0) {
+		if (thisSkillInfo.level.durationLevel > 0 && thisSkillInfo.level.dropletsLevel > 0) {
 			return true;
 		} else {
 			return false;
@@ -80,8 +126,15 @@ public class Skill : MonoBehaviour {
 
 	public virtual bool UnlockSkill(skillID id){
 		if (thisSkillInfo.id == id) {
-			if (thisSkillInfo.level == 0) {
-				thisSkillInfo.level++;
+			if (thisSkillInfo.level.durationLevel == 0) {
+				thisSkillInfo.level.durationLevel++;
+				thisSkillInfo.timer.durationTime = (float)duration_level_time_table [thisSkillInfo.level.durationLevel];
+				thisSkillInfo.level.requiredConinsDurationLvl = (int)duration_level_coin_table [thisSkillInfo.level.durationLevel];
+			}
+			if (thisSkillInfo.level.dropletsLevel == 0) {
+				thisSkillInfo.level.dropletsLevel++;
+				thisSkillInfo.requiredDroplets = (int)droplets_level_drops_table [thisSkillInfo.level.dropletsLevel];
+				thisSkillInfo.level.requiredConinsDurationLvl = (int)droplets_level_coin_table [thisSkillInfo.level.dropletsLevel];
 			}
 			return true;
 		} else {
---
+++ skywalk/Assets/Scripts/SkillConjure.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillConjure : Skill {
+
+	new public skillInfo thisSkillInfo = new skillInfo();
+
+	new public Hashtable duration_level_coin_table = new Hashtable()
+	{// {current level, coins required to next level}
+		{0, 26},
+		{1, 26},
+		{2, 26},
+		{3, 26},
+		{4, 26},
+		{5, 26},
+		{6, 26},
+		{7, 26},
+		{8, 26},
+		{9, 26}
+	};
+
+	new public Hashtable duration_level_time_table = new Hashtable()
+	{// {current level, duration time}
+		{0, 0},
+		{1, 6},
+		{2, 7},
+		{3, 8},
+		{4, 9},
+		{5, 10},
+		{6, 11},
+		{7, 12},
+		{8, 13},
+		{9, 14},
+		{10, 15}
+	};
+
+	new public Hashtable droplets_level_coin_table = new Hashtable()
+	{// {current level, coins required to next level}
+		{0, 26},
+		{1, 26},
+		{2, 26},
+		{3, 26},
+		{4, 26},
+		{5, 26}
+	};
+
+	new public Hashtable droplets_level_drops_table = new Hashtable()
+	{// {current level, droplets to collect}
+		{0, 10000},
+		{1, 5},
+		{2, 4},
+		{3, 3},
+		{4, 2},
+		{5, 1}
+	};
+
+	// Use this for initialization
+	void Start () {
+		thisSkillInfo.id = skillID.Growth;
+		thisSkillInfo.level.durationLevel = 0;
+		thisSkillInfo.level.dropletsLevel = 0;
+		thisSkillInfo.description = "Allowing you to transform objects.";
+		thisSkillInfo.playerOrGame = true;
+		thisSkillInfo.isActivate = false;
+		thisSkillInfo.timer.durationTime = (float)duration_level_time_table [thisSkillInfo.level.durationLevel];
+		thisSkillInfo.requiredDroplets = (int)droplets_level_drops_table [thisSkillInfo.level.dropletsLevel];
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
